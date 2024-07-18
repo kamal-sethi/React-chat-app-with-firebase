41217bf8af86819b87619569d4aa453eed72a39f
kamal-sethi <kamalsethi018@gmail.com>
2024-07-19 00:30:13 +0530

    ui of chatlist is done

---
+++ src/components/details/Detail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './details.css'
+const Detail = () => {
+  return (
+    <div className='detail'>details</div>
+  )
+}
+
+export default Detail
---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import './chatList.css'
+import { useState } from 'react'
+const ChatList = () => {
+    const[addMode,setAddMode]=useState(false)
+  return (
+    <div className='chatList'>
+        <div className="search">
+            <div className="searchbar">
+                <img src="./search.png" alt="" />
+                <input type='text' placeholder='Search'/>
+            </div>
+            <img src={addMode ? "./minus.png" :"./plus.png"} alt="" className='add' onClick={()=>setAddMode((prev)=>!prev)}/>
+        </div>
+        <div className='item'>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane do</span>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='item'>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane do</span>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='item'>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane do</span>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='item'>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane do</span>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='item'>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane do</span>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='item'>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane do</span>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='item'>
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane do</span>
+            <p>hello</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default ChatList
